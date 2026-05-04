d2ff569bac0710b1eb194ef3b7ee69709115c801
kevinrodbe <kevinrodbe@users.noreply.github.com>
2026-05-04 13:06:23 -0500

    feat: use const

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,19 @@
 // tsdown.config.ts
 import { defineConfig } from 'tsdown';
 
+const MY_ENV_1 = process.env['MY_ENV_1'] ?? 'ups';
+const MY_ENV_2 = process.env['MY_ENV_2'] ?? 'upsi';
+
+console.log('[build] MY_ENV_1:', MY_ENV_1);
+console.log('[build] MY_ENV_2:', MY_ENV_2);
+
 // eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   clean: true,
   define: {
     mysingleton: '"hey"',
-    'process.env.MY_ENV_1': JSON.stringify(process.env['MY_ENV_1'] ?? 'ups'),
-    'process.env.MY_ENV_2': JSON.stringify(process.env['MY_ENV_2'] ?? 'upsi'),
+    'process.env.MY_ENV_1': JSON.stringify(MY_ENV_1),
+    'process.env.MY_ENV_2': JSON.stringify(MY_ENV_2),
   },
   dts: true,
   entry: ['./src/index.ts'],
