f56dd87e87a1cdb185a6e38630efeb9e158885cd
kevinrodbe <kevinrodbe@users.noreply.github.com>
2026-05-04 12:52:55 -0500

    feat: add env 2

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { other } from './other';
 
 export const init = () => {
   console.log('init RBX lib', process.env['MY_ENV_1']);
+  console.log('init RBX lib', process.env['MY_ENV_2']);
   console.log('init RBX lib', process.env['MY_SECRET_1']);
 };
 
