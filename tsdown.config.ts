4d024bee5c86e4e670a567433b1a6880de14d5bf
kevinrodbe <kevinrodbe@users.noreply.github.com>
2026-05-04 12:43:51 -0500

    feat: use define for env

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'tsdown';
 // eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   clean: true,
+  define: {
+    mysingleton: '"hey"',
+    'process.env.MY_ENV_1': process.env['MY_ENV_1'] ?? 'ups',
+  },
   dts: true,
   entry: ['./src/index.ts'],
   // envFile: '.env', cuando se quiera usar variables de entorno
